61e77f645f9fcc3d3ed228c0bf31895352a6455b
Erzhan Torokulov <eragon.sbt@gmail.com>
2017-05-06 22:10:30 +0600

    Add CountryService (both Interface and Implementation) #10

---
+++ Heroes.BusinessLogicLayer/Contracts/ICountryService.cs
@@ -0,0 +1,21 @@
+﻿using Heroes.Data.Models;
+using Heroes.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes.BusinessLogicLayer.Contracts
+{
+    public interface ICountryService
+    {
+        IEnumerable<CountryDTO> GetAll(Expression<Func<Country, bool>> filter = null);
+        CountryDTO GetById(int id);
+        void Update(CountryDTO countryToUpdate);
+        CountryDTO Add(CountryDTO countryToAdd);
+        CountryDTO Delete(int id);
+        bool CountryExists(int id);
+    }
+}
---
+++ Heroes.BusinessLogicLayer/Services/CountryService.cs
@@ -0,0 +1,65 @@
+﻿using Heroes.BusinessLogicLayer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Heroes.Data.Models;
+using Heroes.DataAccessLayer.Models;
+using System.Linq.Expressions;
+using Heroes.DataAccessLayer.GenericRepository;
+using Heroes.DataAccessLayer.UnitOfWork;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace Heroes.BusinessLogicLayer.Services
+{
+    public class CountryService : BaseService, ICountryService
+    {
+        private readonly IGenericRepository<Country> countryRepository;
+
+        public CountryService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            countryRepository = this.unitOfWork.GetRepository<Country>();
+        }
+
+        public CountryDTO Add(CountryDTO countryToAdd)
+        {
+            var country = Mapper.Map<Country>(countryToAdd);
+            countryRepository.Insert(country);
+            unitOfWork.Save();
+            return Mapper.Map<CountryDTO>(country);
+        }
+
+        public bool CountryExists(int id)
+        {
+            return countryRepository.GetById(id) != null;
+        }
+
+        public CountryDTO Delete(int id)
+        {
+            var country = countryRepository.GetById(id);
+            countryRepository.Delete(country);
+            unitOfWork.Save();
+            return Mapper.Map<CountryDTO>(country);
+        }
+
+        public IEnumerable<CountryDTO> GetAll(Expression<Func<Country, bool>> filter = null)
+        {
+            return countryRepository.Get(filter).ProjectTo<CountryDTO>();
+        }
+
+        public CountryDTO GetById(int id)
+        {
+            var country = countryRepository.GetById(id);
+            return Mapper.Map<CountryDTO>(country);
+        }
+
+        public void Update(CountryDTO countryToUpdate)
+        {
+            var country = Mapper.Map<Country>(countryToUpdate);
+            countryRepository.Update(country);
+            unitOfWork.Save();
+        }
+    }
+}
