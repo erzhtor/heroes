c40c75da3a9596b0eb05c8bbeadb47431fc2c028
Erzhan Torokulov <eragon.sbt@gmail.com>
2017-05-08 01:07:03 +0600

    Add BusinessLogicLayer Unit Test Project #7; Add UnitTests for CountryService

---
+++ Heroes.BusinessLogicLayer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Heroes.BusinessLogicLayer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Heroes.BusinessLogicLayer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f590ebe-68ed-4874-8c95-7450638f9c84")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
